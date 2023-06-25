5a8011fb4a7263b77eef073f4df30d063cbd6ea4
Amer Mehmedić <harveyboarden@gmail.com>
2023-06-25 20:04:31 +0200

    First commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //  /api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet] // GET
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("{UserId}")] // GET
+        public async Task<ActionResult<AppUser>> GetUser(int UserId)
+        {
+            var user = await _context.Users.FindAsync(UserId);
+            return user;
+        }
+    }
+}
