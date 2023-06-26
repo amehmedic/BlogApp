cf51b808510f932eb36cda9f45b5b882aeda62cf
Amer Mehmedić <harveyboarden@gmail.com>
2023-06-26 16:07:58 +0200

    Added registration and login, authorization and authentication

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,8 +7,12 @@ namespace API.Entities
         [Key]
         public int UserId { get; set; }
 
-        public string? UserName { get; set; }
+        public string UserName { get; set; }
 
-        public string? UserEmail { get; set; }
+        public string UserEmail { get; set; }
+
+        public byte[] PasswordHash {get; set;}
+        
+        public byte[] PasswordSalt {get; set;}
     }
 }
